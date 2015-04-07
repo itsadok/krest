5ed1843cd43893ce39edc55b329530b320e53da8
Zaar Hai <haizaar@haizaar.com>
2015-04-07 11:19:21 +0300

    Added copyright notices

--- krest.py
+++ krest.py
@@ -1,3 +1,11 @@
+#
+# (c) 2015 Kaminario Technologies, Ltd.
+#
+# This software is licensed solely under the terms of the Apache 2.0 license,
+# the text of which is available at http://www.apache.org/licenses/LICENSE-2.0.
+# All disclaimers and limitations of liability set forth in the Apache 2.0 license apply.
+#
+
 from __future__ import absolute_import
 
 __version__ = "1.2.0"
